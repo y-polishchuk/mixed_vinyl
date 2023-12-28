4d56de88589502237a8802cf7a0bc71b5374092f
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-12-28 18:05:26 +0200

    add browse method

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
@@ -12,5 +13,18 @@ class VinylController extends AbstractController
     public function index(): Response
     {
         return new Response('Title: PB and Jams');
+    }
+
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    public function browse(string $slug = null): Response
+    {
+        if($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All Genres';
+        }
+        
+        // return new Response('Breakup vinyl? Angsty 90s rock? Browse the collection!');
+        return new Response($title);
     }
 }
