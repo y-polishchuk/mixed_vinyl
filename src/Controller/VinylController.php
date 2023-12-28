c04a6ea059d6f3efd7fc06550990ffbc975c3620
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-12-28 19:31:01 +0200

    add some Twiggy goodness

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -12,7 +12,18 @@ class VinylController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return new Response('Title: PB and Jams');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey']
+        ];
+        return $this->render('vinyl/index.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
