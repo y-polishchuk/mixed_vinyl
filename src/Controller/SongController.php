3e5a7f857c01dd04ceacd93a5e5ef3a47e059541
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-12-30 11:55:43 +0200

    add route requirement

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SongController extends AbstractController
 {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id): Response
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id): Response
     {
         // TODO query the database
         $song = [
