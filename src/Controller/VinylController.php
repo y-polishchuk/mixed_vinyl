80a969c71a56a2619d4e5c02bde02f408b220546
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-08 21:26:15 +0200

    add DI

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controller;
 
-use Psr\Cache\CacheItemInterface;
+use App\Service\MixRepository;
 use function Symfony\Component\String\u;
-use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VinylController extends AbstractController
@@ -31,14 +29,10 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(HttpClientInterface $httpClient, CacheInterface $cache, string $slug = null): Response
+    public function browse(MixRepository $mixRepository, string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $cache->get('mixes_data', function (CacheItemInterface $cacheItem) use ($httpClient) {
-            $cacheItem->expiresAfter(5);
-            $response = $httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
-            return $response->toArray();
-        });
+        $mixes = $mixRepository->findAll();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
---
+++ src/Service/MixRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Service;
+
+use Psr\Cache\CacheItemInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MixRepository
+{
+  public function __construct(
+    private HttpClientInterface $httpClient, 
+    private CacheInterface $cache
+    )
+  {
+  }
+
+  public function findAll(): array
+  {
+    return $this->cache->get('mixes_data', function (CacheItemInterface $cacheItem) {
+        $cacheItem->expiresAfter(5);
+        $response = $this->httpClient->request('GET', 'https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json');
+        return $response->toArray();
+    });
+  }
+}
