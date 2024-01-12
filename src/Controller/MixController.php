82de84a58f4c64131e6bf2487480d72142412e03
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-12 18:54:28 +0200

    add show page

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,5 +31,14 @@ class MixController extends AbstractController
       $mix->getId(),
       $mix->getTrackCount()
     ));
+  }
+
+  #[Route('/mix/{id}', name: 'app_mix_show')]
+  public function show($id, VinylMixRepository $mixRepository): Response
+  {
+    $mix = $mixRepository->find($id);
+    return $this->render('mix/show.html.twig', [
+      'mix' => $mix,
+    ]);
   }
 }
