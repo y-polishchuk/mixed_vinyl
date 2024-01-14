80a53d7ef079d05e64776c2d7e92e02fe4cdaa37
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-14 15:01:11 +0200

    add sluggable

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -35,7 +35,7 @@ class MixController extends AbstractController
     ));
   }
 
-  #[Route('/mix/{id}', name: 'app_mix_show')]
+  #[Route('/mix/{slug}', name: 'app_mix_show')]
   public function show(VinylMix $mix): Response
   {
     return $this->render('mix/show.html.twig', [
@@ -56,7 +56,7 @@ class MixController extends AbstractController
     $entityManager->flush();
     $this->addFlash('success', 'Vote counted!');
     return $this->redirectToRoute('app_mix_show', [
-      'id' => $mix->getId(),
+      'slug' => $mix->getSlug(),
     ]);
   }
 }
